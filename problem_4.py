b48220b59b8f0d929655e9deb440ef714dcb646f
phocking979 <p_hocking@mail.harpercollege.edu>
2026-01-24 16:28:35 -0600

    Add files via upload
    Solutions for week 2 assignments

---
+++ problem_4.py
@@ -0,0 +1,8 @@
+# Problem 4
+#Allow a user to enter two floating point numerical values. Display the product of the two numbers. 
+
+v1 = float(input("Enter a floating point: "))
+v2 = float(input("Enter another floating point: "))
+
+product = v1 * v2
+print(product)
---
+++ problem_5.py
@@ -0,0 +1,7 @@
+# Problem 5
+#Use the type function to display the data types of a string, float and integer. 
+strEx = "hello"
+intEx = 4
+floatEx = 6.23
+
+print("strEx is type:", type(strEx), "intEx is type:", type(intEx), "floatEx is type:", type(floatEx))
